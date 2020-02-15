9156eaff42debfbc111eacc99353e25844ceb173
Louise Seema <lseema@il-i3.21-school.ru>
2020-02-15 16:51:38 +0300

    add strings, pointers

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: lseema <lseema@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/09 20:09:10 by lseema            #+#    #+#             */
-/*   Updated: 2020/02/14 19:32:38 by lseema           ###   ########.fr       */
+/*   Updated: 2020/02/15 16:50:53 by lseema           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,51 +16,53 @@
 # include <stdarg.h>
 # include "libft/libft.h"
 
-typedef struct  s_format
+typedef struct		s_format
 {
-    //Индекс обрабатываемого символа формата
-    size_t  i;
-    //Флаг хэша
-    char    hash;
-    //Флаг -, 0
-    char    minus_zero;
-    //Флаг + или space
-    char    plus_space;
-    //Ширина
-    size_t  width;
-    //Точность
-    int     precision;
-    //Размер
-    int     size;
-    char    type;
-    char    float_type;
-}               t_format;
+	//Индекс обрабатываемого символа формата
+	size_t	i;
+	//Флаг хэша
+	char	hash;
+	//Флаг -, 0
+	char	minus_zero;
+	//Флаг + или space
+	char	plus_space;
+	//Ширина
+	size_t	width;
+	//Точность
+	int	precision;
+	//Размер
+	int		size;
+	char	type;
+	char	float_type;
+}					t_format;
 
-int         ft_printf(const char *format, ...);
-size_t      format_manager(const char *format, va_list ap);
-void        format_clean(t_format *param);
-t_format    *format_initialize(void);
-void		format_parser(const char *format, t_format *param, va_list ap);
-void		flags_parser(const char *format, t_format *param);
-void        width_prcsn_parser(const char *format, t_format *param, va_list ap);
-void        size_parser(const char *format, t_format *param);
-size_t		print_argument(t_format *param, char s, va_list ap);
-size_t		print_int(t_format *param, va_list ap);
-size_t		print_oct(t_format *param, va_list ap);
-size_t		print_unsigned(t_format *param, va_list ap);
-size_t		print_hex(t_format *param, va_list ap);
-// size_t		print_str(t_format *param, va_list ap);
-// size_t		print_pointer(t_format *param, va_list ap);
-size_t		print_percent(t_format *param);
-size_t		print_chr(t_format *param, va_list ap);
+int					ft_printf(const char *format, ...);
+size_t				format_manager(const char *format, va_list ap);
+void				format_clean(t_format *param);
+t_format			*format_initialize(void);
+void				format_parser(const char *format, t_format *param, va_list ap);
+void				flags_parser(const char *format, t_format *param);
+void				width_prcsn_parser(const char *format, t_format *param, va_list ap);
+void				size_parser(const char *format, t_format *param);
+size_t				print_argument(t_format *param, char s, va_list ap);
+size_t				print_int(t_format *param, va_list ap);
+size_t				print_oct(t_format *param, va_list ap);
+size_t				print_unsigned(t_format *param, va_list ap);
+size_t				print_hex(t_format *param, va_list ap);
+size_t				print_str(t_format *param, va_list ap);
+size_t				print_pointer(t_format *param, va_list ap);
+size_t				print_percent(t_format *param);
+size_t				print_chr(t_format *param, va_list ap);
 // size_t		print_float(t_format *param, va_list ap);
-char		*ft_itoa_base(unsigned long long value, int base, int reg);
-char		*print_int2(t_format *param, char *str, int sign);
-char		*fill_zero(t_format *param, char *str, size_t len);
-char	    *fill_zero_unsigned(t_format *params, char *str, size_t len);
-char		*print_width(t_format *params, char *str, size_t len);
-char		*add_atributes_width(t_format *params, char *str, char *tmp);
-char		*split_and_assemble(t_format *params, char *str, char *tmp);
-char		*add_atributes_precision(char *tmp, char *str);
+char				*ft_itoa_base(unsigned long long value, int base, int reg);
+char				*print_int2(t_format *param, char *str, int sign);
+char				*fill_zero(t_format *param, char *str, size_t len);
+char				*fill_zero_unsigned(t_format *params, char *str, size_t len);
+char				*print_width(t_format *params, char *str, size_t len);
+char				*add_atributes_width(t_format *params, char *str, char *tmp);
+char				*split_and_assemble(t_format *params, char *str, char *tmp);
+char				*add_atributes_precision(char *tmp, char *str);
 unsigned long long	get_unsigned(t_format *param, va_list ap);
+char				*string_width(t_format *param, char *str, size_t len);
+char				*pointer_precision(t_format *param, char *str, size_t len);
 #endif
--- print_symbolic.c
+++ print_symbolic.c
@@ -6,7 +6,7 @@
 /*   By: lseema <lseema@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/12 18:55:26 by lseema            #+#    #+#             */
-/*   Updated: 2020/02/14 19:31:56 by lseema           ###   ########.fr       */
+/*   Updated: 2020/02/15 16:42:31 by lseema           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,4 +56,91 @@ size_t		print_chr(t_format *param, va_list ap)
 	else
 		count = write(1, &arg, 1);
 	return (count);
+}
+
+size_t		print_str(t_format *param, va_list ap)
+{
+	char *arg;
+	char *new;
+	size_t len;
+
+	if (!(arg = va_arg(ap, char*)))
+		arg = ft_strdup("(null)");
+	if (param->precision != -1 && param->precision < (int)ft_strlen(arg))
+	{
+		len = 0;
+		new = ft_strnew(param->precision);
+		while (param->precision > len)
+		{
+			new[len] = arg[len];
+			len++;
+		}
+		ft_strdel(&arg);
+		arg = ft_strdup(new);
+		ft_strdel(&new);
+	}
+	len = ft_strlen(arg);
+	if (param->width > len)
+		arg = string_width(param, arg, len);
+	len = write(1, arg, ft_strlen(arg));
+	free(arg);
+	return len;
+}
+
+char		*string_width(t_format *param, char *str, size_t len)
+{
+	char	*tmp;
+	char	*new;
+	size_t	i;
+
+	i = 0;
+	tmp = ft_strnew(param->width - len);
+	while ((int)param->width - len > i)
+		tmp[i++] = (param->minus_zero != '0') ? ' ' : '0';
+	new = (param->minus_zero == '-')
+		? ft_strjoin(str, tmp) : ft_strjoin(tmp, str);
+	ft_strdel(&tmp);
+	ft_strdel(&str);
+	return (new);
+}
+
+size_t		print_pointer(t_format *param, va_list ap)
+{
+	unsigned long long int	arg;
+	char					*tmp;
+	char					*str;
+	size_t					len;
+
+	arg = (unsigned long long int)va_arg(ap, void *);
+	if (!param->precision && !arg)
+		str = ft_strdup("0x");
+	else
+	{
+		tmp = ft_itoa_base(arg, 16, 0);
+		str = ft_strjoin("0x", tmp);
+		ft_strdel(&tmp);
+	}
+	if (param->precision > (int)(len = ft_strlen(str)))
+		str = pointer_precision(param, str, len);
+	str = print_width(param, str, ft_strlen(str));
+	len = write(1, str, ft_strlen(str));
+	free(str);
+	return (len);
+}
+
+char		*pointer_precision(t_format *param, char *str, size_t len)
+{
+	char	*tmp;
+	char	*new;
+	int		i;
+
+	i = 0;
+	tmp = ft_strnew(param->precision - len + 2);
+	while (param->precision - (int)len + 2)
+		tmp[i++] = '0';
+	new = (param->width <= (ft_strlen(tmp) + len))
+		? split_and_assemble(param, str, tmp) : ft_strjoin(tmp, str);
+	ft_strdel(&tmp);
+	ft_strdel(&str);
+	return(new);
 }
